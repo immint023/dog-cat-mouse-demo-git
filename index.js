617d6ce99390be47466e70c775cbe98baf1ffefb
MinhNe <nvminh023@gmail.com>
2018-09-16 23:13:54 +0700

    dog eat cat

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var Dog = require('./Dog.js');
-var so = new Dog('so');
-so.sayHi();
+var Cat = require('./Cat.js');
+
+var dog = new Dog('Tom');
+var tom = new Cat();
+
+dog.eat(tom);
+console.log(dog);
